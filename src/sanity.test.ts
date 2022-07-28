1da555ba89ec39bcf620d423d684c49991aa9b0b
Benjamin Laird <86321957+ben-laird@users.noreply.github.com>
2022-07-28 16:13:30 -0500

    test: :white_check_mark: add sanity test
    Add a sanity test to ensure AVA works

---
+++ src/sanity.test.ts
@@ -0,0 +1,8 @@
+import test from "ava";
+
+/* I generally keep this sanity test file around just to integration test AVA.
+That way I know it's me that messed up tests lol */
+
+test("Passing sanity test", (t) => {
+  t.pass("Sanity test passed!");
+});
