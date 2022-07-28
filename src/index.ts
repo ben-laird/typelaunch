c767beaef9c7d1c6ca7c98f9838a074ba9d89b0a
Benjamin Laird <86321957+ben-laird@users.noreply.github.com>
2022-07-28 16:27:55 -0500

    feat(index): :white_check_mark: implement greet function
    Pass index tests by implementing greet function

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+/**
+ * A simple function meant to demonstrate all the features of TypeLaunch, from testing to docs to more!
+ * This simply greets the user in the console and returns what it sent to the console.
+ * @param name The name of the person you'd like to greet
+ * @returns A string of the greeting with the name inserted
+ */
+export const greet = (name: string) => {
+  const message = `Hello there, ${name}`;
+  console.log(message); // TODO take a look at Todo Tree!
+  return message;
+};
