e6ce47c0fce0dfc06ff11161d5234f4e8f51d6c5
Benjamin Laird <86321957+ben-laird@users.noreply.github.com>
2023-02-18 03:25:57 -0500

    test: :white_check_mark: migrate tests to vitest
    Migrate tests to vitest and demonstrate in-source testing

--- src/index.ts
+++ src/index.ts
@@ -9,3 +9,17 @@ export const greet = (name: string) => {
   console.log(message); // TODO take a look at Todo Tree!
   return message;
 };
+
+if (import.meta.vitest) {
+  const { it, expect } = import.meta.vitest;
+
+  const greetTest = (name: string) => {
+    it(`Greets ${name}`, () => {
+      expect(greet(name)).toBe(`Hello there, ${name}`);
+    });
+  };
+
+  greetTest("Angie");
+  greetTest("Janet");
+  greetTest("Marcos");
+}
--- src/sanity.test.ts
+++ src/sanity.test.ts
@@ -1,8 +1,8 @@
-import test from "ava";
+import { describe, it } from "vitest";
 
-/* I generally keep this sanity test file around just to integration test AVA.
+/* I generally keep this sanity test file around just to integrate-test Vitest.
 That way I know it's me that messed up tests lol */
 
-test("Passing sanity test", (t) => {
-  t.pass("Sanity test passed!");
+describe("Passing sanity test", () => {
+  it("Should just pass");
 });
